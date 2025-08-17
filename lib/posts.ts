996c184d54d0d0e14360ec85eb4661685f116878
s-hayashi123 <shake7602@gmail.com>
2025-08-17 22:23:45 +0900

    ソート修正

--- lib/posts.ts
+++ lib/posts.ts
@@ -29,11 +29,7 @@ export function getSortedPostsData() {
   });
 
   return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
   });
 }
 
